0a4ee3e117a92e7f3eb2485e7da6ace8c9934de8
Nidhi Kumari <nidhi1691996@outlook.com>
2020-07-18 13:09:23 +0530

    charts added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const theme = createMuiTheme();
+
 ReactDOM.render(
-  <React.StrictMode>
+  <MuiThemeProvider theme={theme}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </MuiThemeProvider>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
